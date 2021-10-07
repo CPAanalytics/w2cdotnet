852b965108954530c70ed7ff2385d2835e9e2407
Joe Chevalier <79522568+CPAanalytics@users.noreply.github.com>
2021-10-06 19:06:23 -0700

    First test of writeline

---
+++ w2cdotnet_test/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using w2cdotnet;
+
+namespace w2cdotnet_test
+{
+    [TestClass]
+    public class EINs
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            var testvalidein = new Submitter(123456789);
+            testvalidein.WriteLine();
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidEin))]
+        public void TestTooShort()
+        {
+            List<int> testlist = new List<int>{123, 123456789, 283456789};
+            
+            foreach(int field in testlist)
+            {
+                var einlist = new Submitter(field);
+                Assert.ThrowsException<Exceptions.InvalidEin>(() => einlist.WriteLine());
+            }
+        
+            
+        }
+    }
+}
